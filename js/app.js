9df75708e64e2de039fa2d86ef56eec75d77fc2b
jlboyer <jlboyer1@gmail.com>
2019-07-08 18:37:56 -0500

    planet class defined

--- js/app.js
+++ js/app.js
@@ -2,9 +2,16 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 class Planet = {
-  centerX: 0;
-  centerY: 0;
-  radius: Math.random()*10 + 10;
-  gravity: 
-
+  constructor(){
+    this.centerX: 0;
+    this.centerY: 0;
+    this.radius: Math.random()*10 + 10;
+    this.attractF: 10;
+  } 
+  draw(){
+    ctx.beginPath()
+    ctx.arc(this.centerX,this.centerY,this.radius,0,2*Math.PI,false)
+    ctx.stroke()
+  };
 }
+
