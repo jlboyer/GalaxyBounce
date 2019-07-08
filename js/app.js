ddac9fb4470a50670caf9de468ae55f3e9966f0a
jlboyer <jlboyer1@gmail.com>
2019-07-08 17:31:21 -0500

    planet class started

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+class Planet = {
+  centerX: 0;
+  centerY: 0;
+  radius: Math.random()*10 + 10;
+  gravity: 
+
+}
