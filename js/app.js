dc61a3e9ce18b512c2def86e098d9ced3171ac6a
jlboyer <jlboyer1@gmail.com>
2019-07-08 19:35:07 -0500

    track mouse postion over canvas

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-class Planet = {
+class Planet {
   constructor(){
-    this.centerX: 0;
-    this.centerY: 0;
-    this.radius: Math.random()*10 + 10;
-    this.attractF: 10;
+    this.centerX = 0;
+    this.centerY = 0;
+    this.radius = Math.random()*10 + 10;
+    this.attractF = 10;
   } 
   draw(){
     ctx.beginPath()
@@ -15,3 +15,27 @@ class Planet = {
   };
 }
 
+class PlayerShip {
+  constructor(){
+    this.centerX = 0;
+    this.centerY = 0;
+    this.radius = Math.random()*10 + 10;
+    this.attractF = 10;
+  }
+}
+
+// const canvasElement = document.getElementsByTagName("canvas")
+
+document.onmousemove = (evt) =>{
+  console.log(evt)
+  console.log(getMousePos(canvas, evt).x)
+  console.log(getMousePos(canvas, evt).y)
+}
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
