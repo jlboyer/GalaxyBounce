1a457c105c405f19a6661c249ca7a3c6885ff033
jlboyer <jlboyer1@gmail.com>
2019-07-08 21:25:14 -0500

    refactored code with game object

--- js/app.js
+++ js/app.js
@@ -18,10 +18,11 @@ class Planet {
 }
 
 class PlayerShip {
-  constructor(){
-    this.centerX = 0;
-    this.centerY = 0;
+  constructor(x , y){
+    this.centerX = x;
+    this.centerY = y;
     this.radius = 10;
+    this.velocity = 10;
     // this.repulseF = 10; not sure if i need this right now
   }
   draw(){
@@ -33,24 +34,44 @@ class PlayerShip {
   }
 }
 
-planetA = new Planet(300,300)
 
-function animate() {
-  clearCanvas();
-  planetA.draw()
-  console.log(planetA.centerX)
-  window.requestAnimationFrame(animate)
+
+const game = {
+  objects: [],
+  initialize(){
+    let planetA = new Planet(300,300)
+    this.objects.push(planetA)
+
+    document.onmousemove = (evt) =>{
+      game.objects[0].centerX = getMousePos(canvas, evt).x
+      game.objects[0].centerY= getMousePos(canvas, evt).y
+    }
+
+    this.animate()
+  },
+  animate(){
+    game.clearCanvas()
+    game.objects.forEach(object => {
+      object.draw()
+    })
+    window.requestAnimationFrame(game.animate)
+  },
+  clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)  
+  }
 }
-animate()
 
 
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)  
-}
+game.initialize()
 
-document.onmousemove = (evt) =>{
-  planetA.centerX = getMousePos(canvas, evt).x
-  planetA.centerY= getMousePos(canvas, evt).y
+
+
+document.onclick = (evt) =>{
+  let shipA = new PlayerShip(getMousePos(canvas, evt).x + planetA.radius*3,getMousePos(canvas, evt).y + planetA.radius*3)
+  console.log(shipA)
+  console.log(getMousePos(canvas, evt).x + planetA.radius*3)
+  console.log(getMousePos(canvas, evt).y + planetA.radius*3)
+  shipA.draw()
 }
 
 function getMousePos(canvas, evt) {
