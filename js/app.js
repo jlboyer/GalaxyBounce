5ed4664cb3e08f1e285c8b426f7d1c14150880d9
jlboyer <jlboyer1@gmail.com>
2019-07-08 20:12:56 -0500

    planet tracks mouse position

--- js/app.js
+++ js/app.js
@@ -21,21 +21,40 @@ class PlayerShip {
   constructor(){
     this.centerX = 0;
     this.centerY = 0;
-    this.radius = Math.random()*10 + 10;
+    this.radius = 10;
     // this.repulseF = 10; not sure if i need this right now
+  }
+  draw(){
+    ctx.beginPath()
+    ctx.arc(this.centerX,this.centerY,this.radius,0,2*Math.PI,false)
+    ctx.strokeStyle = 'rgba(150, 150, 150, 1)';
+    ctx.lineWidth = 2
+    ctx.stroke()
   }
 }
 
 planetA = new Planet(300,300)
-planetA.draw()
+
+function animate() {
+  clearCanvas();
+  planetA.draw()
+  console.log(planetA.centerX)
+  window.requestAnimationFrame(animate)
+}
+animate()
+
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)  
+}
 
 document.onmousemove = (evt) =>{
-  // console.log(getMousePos(canvas, evt).x)
-  // console.log(getMousePos(canvas, evt).y)
+  planetA.centerX = getMousePos(canvas, evt).x
+  planetA.centerY= getMousePos(canvas, evt).y
 }
 
 function getMousePos(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
+  let rect = canvas.getBoundingClientRect();
   return {
     x: evt.clientX - rect.left,
     y: evt.clientY - rect.top
