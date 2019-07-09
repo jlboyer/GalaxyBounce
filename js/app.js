4e8d66508b3079688867bfdfc9acd1a063bca8a5
jlboyer <jlboyer1@gmail.com>
2019-07-08 19:52:18 -0500

    planet draws itself

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 class Planet {
-  constructor(){
-    this.centerX = 0;
-    this.centerY = 0;
+  constructor(x, y){
+    this.centerX = x;
+    this.centerY = y;
     this.radius = Math.random()*10 + 10;
     this.attractF = 10;
   } 
   draw(){
     ctx.beginPath()
     ctx.arc(this.centerX,this.centerY,this.radius,0,2*Math.PI,false)
+    ctx.strokeStyle = 'rgba(150, 150, 150, 1)';
+    ctx.lineWidth = 2
     ctx.stroke()
   };
 }
@@ -20,16 +22,16 @@ class PlayerShip {
     this.centerX = 0;
     this.centerY = 0;
     this.radius = Math.random()*10 + 10;
-    this.attractF = 10;
+    // this.repulseF = 10; not sure if i need this right now
   }
 }
 
-// const canvasElement = document.getElementsByTagName("canvas")
+planetA = new Planet(300,300)
+planetA.draw()
 
 document.onmousemove = (evt) =>{
-  console.log(evt)
-  console.log(getMousePos(canvas, evt).x)
-  console.log(getMousePos(canvas, evt).y)
+  // console.log(getMousePos(canvas, evt).x)
+  // console.log(getMousePos(canvas, evt).y)
 }
 
 function getMousePos(canvas, evt) {
